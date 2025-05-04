360d8642968520319ef27e193dbdcb19755a0197
daniela <danielamc-14@hotmail.com>
2025-05-04 17:06:52 +1000

    segundo cambio

--- notebooks/app.py
+++ notebooks/app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
      
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+df = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
     
 if hist_button: # al hacer clic en el botón
@@ -10,7 +10,8 @@ if hist_button: # al hacer clic en el botón
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
          
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(df, x="odometer")
      
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+    
